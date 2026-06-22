cf7cfe83b178f6a2e8c4abe02f536ecfb3a8e1f8
Curtis Rubeck <rubeckcurtis@gmail.com>
2026-06-22 15:18:44 -0500

    stow pkg layout

--- nvim/.config/nvim/lua/curtrube/keymaps.lua
+++ nvim/.config/nvim/lua/curtrube/keymaps.lua
@@ -45,6 +45,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Diagnostic keymaps
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
 -- native undotree
 vim.keymap.set("n", "<leader>u", function()
     vim.cmd.packadd("nvim.undotree")
